223c3f0b94e1a87399f049a37b07290b7343493a
sayakascsk <sayaka.yanagi@scskeu.com>
2025-11-15 21:20:10 +0000

    Add get_customer_peppol_id()

--- utils/utils.py
+++ utils/utils.py
@@ -1,17 +1,12 @@
 from sqlalchemy import create_engine
-import urllib
 import os
+import pandas as pd
 from dotenv import load_dotenv
 load_dotenv()
 
-user = os.environ['DB_USER']
-password = urllib.parse.quote_plus(os.environ['DB_PASSWORD'])
-host = os.environ['DB_HOST']
-port = os.environ['DB_PORT']
-database = os.environ['DB_DATABASE']
 
 
-def get_engine():
+def get_engine(user, password, database, host, port):
 
     """
     Connect to SQLServer database
@@ -21,7 +16,68 @@ def get_engine():
     print(connect_string)
     try:
         engine = create_engine(connect_string, echo=False)
+        print(f'Connected to {host}:{database}')
         return engine
     except Exception as e:
         print(f'Failed to connect {host}:{database}')
         raise
+
+def get_storecove_credentials(branch_code):
+
+    """
+    Get Legal Entity ID and API Key of Storecove API
+    """
+    load_dotenv()
+
+    try:
+        api_key = os.environ[f'API_KEY_{branch_code}']
+        legal_entity_id = os.environ[f'LEGAL_ENTITY_ID_{branch_code}']
+        return api_key, legal_entity_id
+    except Exception as e:
+        print(f'{type(e)} : {e}')
+        raise
+
+def get_customer_peppol_id(engine, branch_code, customer_supplier_code, vat_id):
+
+    """
+    Get PEPPOL scheme and id for a customer
+    """
+    target_table = 'customer'
+
+    try:
+        df = pd.read_sql(f'''SELECT peppol_scheme, peppol_id 
+                        FROM {target_table} 
+                        WHERE branch_code = '{branch_code}, {target_table}_code = {customer_supplier_code} AND vat_id = {vat_id}'''
+                        , con=engine)
+    except Exception as e:
+        print(f'{type(e) : {e}}')
+        raise
+    
+    if len(df) == 0:
+        print('There is no customer/supplier')
+        raise
+    else:
+        peppol_scheme = df['peppol_scheme'][0]
+        peppol_id = df['peppol_id'][0]
+
+    return peppol_scheme, peppol_id
+
+
+
+
+# def get_container_client(connection_string, container):
+    
+#     """Get container client of Azure blob storage"""
+
+#     # Connect to Azure blob storage account
+#     try:
+#         blob_service_client = BlobServiceClient.from_connection_string(
+#             connection_string
+#         )
+#     except Exception as e:
+#         logging.error(f"Issue in connection string of Azure storage. {type(e)} {e}")
+#         raise
+
+#     container_client = blob_service_client.get_container_client(container)
+
+#     return container_client
