9664b455706854b702383ba30e1c433726fb9f59
sayakascsk <sayaka.yanagi@scskeu.com>
2025-11-16 15:47:53 +0000

    Combine sql tables for customer and supplier

--- sql/create_customer_supplier.sql
+++ sql/create_customer_supplier.sql
@@ -1,19 +1,19 @@
-CREATE TABLE customer (
+CREATE TABLE customer_supplier (
     company_code VARCHAR(5),
     branch_code VARCHAR(5) NOT NULL,
     vat_id VARCHAR(30) NOT NULL,
-    customer_code CHAR(3) NOT NULL,
-    customer_name VARCHAR(100),
+    code VARCHAR(5) NOT NULL,
+    name VARCHAR(100),
     country_code CHAR(2),
+    b2x CHAR(1),
+    peppol_scheme VARCHAR(15),
     peppol_id VARCHAR(30),
     sending_method VARCHAR(20),
     peppol_available CHAR(1),
     last_peppol_check TIMESTAMP,
     primary_email_address VARCHAR(300),
     secondary_email_addresses TEXT,
+    sipplier_flag CHAR(1),
     last_update TIMESTAMP NOT NULL,
-    b2x CHAR(1),
-    peppol_scheme VARCHAR(15),
-    PRIMARY KEY (branch_code, vat_id, customer_code)
+    PRIMARY KEY (branch_code, vat_id, code)
 );
-
